2e178b8ef900cf613e932174434740da3ba460c1
suvajit <suvajitsamanta42@gmail.com>
2025-10-05 18:42:17 +0530

    today i am going to push Day02 files

---
+++ Today/calculator/mul.js
@@ -0,0 +1,6 @@
+function mul(a,b){
+    console.log(a*b);
+}
+//console.log(module)
+
+module.exports = mul;
---
+++ Today/first.js
@@ -0,0 +1,8 @@
+const {sum, sub, mul} = require("./calculator");
+//by default it fetches from the index.js file, there is no need to specify the index.js file when all the current files are in the same folder
+
+sum(2,4);
+sub(3,4);
+mul(6,8);
+
+console.log("say hello to the world");
