4640562eb695cad7af65d81ad2167479431e064e
suvajit samanta <166320455+suvajit2005@users.noreply.github.com>
2025-10-11 14:13:28 +0530

    Day05 index uploaded

--- index.js
+++ index.js
@@ -1,40 +1,49 @@
-// const http = require('http');
+// const hello = require('http');
 
+// //  How to create a http server
+// //we import the node js predefined module 'http' that mainly used to acctept reqest, give responce, as well as connnect to other server and create server
 
-// const server = http.createServer((req,res)=>{
-    
-//     // res.end("Hello Coder Army");
+// console.log("I am first");
 
+// const server = hello.createServer((req,res)=>{
+//     // the above line we use to create the htttp server using node js
+   
 //     if(req.url==="/"){
-//         res.end("Hello Coder Army");
-//     }
-//     else if(req.url==="/contact"){
-//         res.end("This is our Contact Page")
+//         console.log(req.url)
+//         res.end("Hello BHAI");
 //     }
 //     else if(req.url==="/about"){
-//         res.end("This is our About Page")
+//         console.log(req.url)
+//         res.end("This is about Page");
 //     }
-//     else
-//     {
-//         res.end("Error: Page Not Found");
+//     else if(req.url==="/contact")
+        
+//     {    console.log(req.url)
+//         res.end("Contact info of user")
+//     }
+//     else{
+//         res.end("Page not Found");
 //     }
 // });
 
+// server.listen(2000, ()=>{
+//     console.log("Server running");
+//     // console.log(http)
+//     //console.log(server)
+// });
 
-// server.listen(4000, ()=>{
-//     console.log("I am Listening at port number 4000");
-// })
-///////////////////////////HOW TO BUILD SERVER//////////////////////////////////
+// console.log("I am last");
 
-const http = require('http');
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        //res.writeHead(200, { "Content-Type": "application/json" }); 
-        res.end({"name":"Rohit", "age":20, "money":70, "Mon":20});
-    }
-});
+//////////////////////////////////////////////////////////////////////////////////
 
-server.listen(4000, () => {
-    console.log("hello brother i am listining you don't worry");
-});
+const server=require('http');
+//const { request } = require('undici-types');
+const main=server.createServer((req, res)=>{
+    if(req.url=="/")
+        res.end("hello bhai")
+})
+main.listen(2000,()=>{
+    console.log("helllo hi")
+})
+//er diagram, entity, relation, key , er diagram to table , types of relation ship, entity types, function depenedency types, reflex, amstrong, normalizaion, minimization, 
