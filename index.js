a2c31becfff6f57a40e75918743c8055661678d5
suvajit samanta <166320455+suvajit2005@users.noreply.github.com>
2025-10-13 23:47:22 +0530

    Day07 index.js file uploaded

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 const express = require("express");
-
-// 
 const app = express();
 
-// "/detail"
-// "/contact/person"
-// "/detail/person"
-// "/about"
-// "/detail/home/10"
 
-// ? char become Optional
-// + char can be repeated multiple times
-// * any number of character can arrive
-//  rohit_negi9
+const BookStore = [
+    {id:1,name:"Harry Potter", author:"DevFlux"},
+    {id:2, name:"Friends", author: "Vikas"},
+    {id:3 , name:"Nexus", author:"Rohit"},
+    {id:4 , name:"DSA", author:"Maharaj"},
+    {id:5, name:"Prem Kahani", author:"Rohan"}
+]
 
-app.use("/about/:id", (req,res)=>{
-        console.log(req.params);
-        res.send({name:"Rohit", "age":20, "money":70, "Mon":20, req:`${req.params.id} bheja tha tu`});
+app.use(express.json());
+
+// localhost:3000/book/3
+
+app.get("/book", (req,res)=>{
+
+    res.send(BookStore);
+})
+
+app.get("/book/:id", (req,res)=>{
+
+    const id = parseInt(req.params.id);
+    // console.log(typeof req.params.id)
+    const Book =  BookStore.find((info)=> info.id===id);
+    res.send(Book); 
+})
+
+app.post("/book", (req,res)=>{
+    console.log(req.body);
+    BookStore.push(req.body);
+    res.send("Data Saved Successfully");
 })
 
 
@@ -24,30 +38,46 @@ app.use("/about/:id", (req,res)=>{
 
 
 
-
-
-app.use("/about", (req,res)=>{
-    res.send({"name":"suvajit", "age":20, "money":70, "Mon":20});
-})
-
-app.use("/contact", (req,res)=>{
-    res.send("I am your Contact Page");
-})
-
-app.use("/detail", (req,res)=>{
-    res.send("I am your Detail Page");
-})
-
-app.use("/", (req,res)=>{
-    res.send("I am Your Home Page");
+app.listen(3000, ()=>{
+    console.log("Listening at port 3000");
 })
 
 
+// // app.use("/user", (req,res)=>{
+
+// //     res.send({name:"Rohit"})
+// // })
 
 
-app.listen(4000, ()=>{
-    console.log("Listening at port 4000");
-})
+// // parsing karni hoti hai
+// app.use(express.json()); 
+// //  middleware: json format data=> JS Object 
+
+// app.get("/user", (req,res)=>{
+//     // console.log(req);
+
+//     res.send({name:"Rohit"})
+// })
+
+// app.post("/user", (req,res)=>{
+
+//     // console.log("Data saved successfully");
+
+//     console.log(typeof req.body.age);
+//     res.send("Data Saved Successfully");
+// })
 
 
 
+// get, post, patch, put , delete
+
+
+// app.listen(4000, ()=>{
+//     console.log("Listening at port 4000");
+// })
+
+
+
+// route match honge: app.use
+
+// app.get app.post app.patch app.put app.delete
