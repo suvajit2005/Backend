3ff722e6c9ad0c0d64b4f34bccc61df4a3f40e8b
suvajit samanta <166320455+suvajit2005@users.noreply.github.com>
2025-10-16 14:32:36 +0530

    day 08 index.json uploaded

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const BookStore = [
     {id:2, name:"Friends", author: "Vikas"},
     {id:3 , name:"Nexus", author:"Rohit"},
     {id:4 , name:"DSA", author:"Maharaj"},
-    {id:5, name:"Prem Kahani", author:"Rohan"}
+    {id:5, name:"Prem Kahani", author:"Rohan"},
+    {id:6, name:"Heloo", author: "Vikas"},
 ]
 
 app.use(express.json());
@@ -15,7 +16,9 @@ app.use(express.json());
 // localhost:3000/book/3
 
 app.get("/book", (req,res)=>{
+    console.log(req.query);
 
+    //const Book = BookStore.filter(info => info.author === req.query.author);
     res.send(BookStore);
 })
 
@@ -23,7 +26,7 @@ app.get("/book/:id", (req,res)=>{
 
     const id = parseInt(req.params.id);
     // console.log(typeof req.params.id)
-    const Book =  BookStore.find((info)=> info.id===id);
+    const Book =  BookStore.find(info=> info.id===id);
     res.send(Book); 
 })
 
@@ -32,6 +35,46 @@ app.post("/book", (req,res)=>{
     BookStore.push(req.body);
     res.send("Data Saved Successfully");
 })
+
+app.patch("/book", (req,res)=>{
+    console.log(req.body);
+
+   const Book = BookStore.find(info => info.id === req.body.id);
+   
+   if(req.body.author)
+   Book.author = req.body.author;
+
+   if(req.body.name)
+   Book.name = req.body.name;
+
+   res.send("Patch updated")
+})
+
+
+app.put("/book", (req,res)=>{
+
+    const Book = BookStore.find(info => info.id === req.body.id);
+    
+    Book.author = req.body.author;
+    Book.name = req.body.name;
+
+    res.send("Changes Updated Succesfully");
+})
+
+app.delete("/book/:id", (req,res)=>{
+
+    const id = parseInt(req.params.id);
+    
+ 
+    const index = BookStore.findIndex(info => info.id === id);
+    
+    console.log(index);
+    BookStore.splice(index,1);
+    res.send("Successfully Deleted");
+
+})
+
+
 
 
 
@@ -42,42 +85,3 @@ app.listen(3000, ()=>{
     console.log("Listening at port 3000");
 })
 
-
-// // app.use("/user", (req,res)=>{
-
-// //     res.send({name:"Rohit"})
-// // })
-
-
-// // parsing karni hoti hai
-// app.use(express.json()); 
-// //  middleware: json format data=> JS Object 
-
-// app.get("/user", (req,res)=>{
-//     // console.log(req);
-
-//     res.send({name:"Rohit"})
-// })
-
-// app.post("/user", (req,res)=>{
-
-//     // console.log("Data saved successfully");
-
-//     console.log(typeof req.body.age);
-//     res.send("Data Saved Successfully");
-// })
-
-
-
-// get, post, patch, put , delete
-
-
-// app.listen(4000, ()=>{
-//     console.log("Listening at port 4000");
-// })
-
-
-
-// route match honge: app.use
-
-// app.get app.post app.patch app.put app.delete
